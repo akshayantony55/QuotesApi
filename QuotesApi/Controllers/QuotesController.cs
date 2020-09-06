adfba65017c0d726fd4a56d1c6dcb4dc2355d4ec
Akshay Antony <akshaya@valorem.com>
2020-09-06 19:58:45 +0530

    first commit

---
+++ QuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using QuotesApi.Data;
+using QuotesApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private QuotesDbContext _quotesDbContext;
+
+        public QuotesController(QuotesDbContext quotesDbContext)
+        {
+            _quotesDbContext = quotesDbContext;
+        }
+        // GET: api/<QuotesController>
+        [HttpGet]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
+        public IActionResult Get(string sort)
+        {
+            IQueryable<Quote> quotes;
+            switch (sort)
+            {
+                case "desc":
+                    quotes = _quotesDbContext.Quotes.OrderByDescending(q => q.CreatedAt);
+                    break;
+                case "asc":
+                    quotes = _quotesDbContext.Quotes.OrderBy(q => q.CreatedAt);
+                    break;
+                default:
+                    quotes = _quotesDbContext.Quotes;
+                    break;
+            }
+            return Ok(quotes);
+        }
+        [HttpGet("[action]")]
+        public ActionResult PagingQuote(int? pageNumber, int? pageSize)
+        {
+            var quotes = _quotesDbContext.Quotes;
+            var currentPageNumber = pageNumber ?? 1;
+            var currentPageSize = pageSize ?? 5;
+            return Ok(quotes.Skip((currentPageNumber - 1) * currentPageSize).Take(currentPageSize));
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult SearchQuote(string type)
+        {
+            var quotes = _quotesDbContext.Quotes.Where(q=>q.Type.StartsWith(type));
+            return Ok(quotes);
+        }
+        // GET api/<QuotesController>/5
+        [HttpGet("{id}")]
+        [Authorize]
+        public Quote Get(int id)
+        {
+            var quotes = _quotesDbContext.Quotes.Find(id);
+            return quotes;
+        }
+        //api/quotes/test/id
+        [HttpGet("[action]/{id}")]
+        public int Test(int id)
+        {
+            return id;
+        }
+        // POST api/<QuotesController>
+        [HttpPost]
+        public void Post([FromBody] Quote quote)
+        {
+            _quotesDbContext.Quotes.Add(quote);
+            _quotesDbContext.SaveChanges();
+        }
+
+        // PUT api/<QuotesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Quote quote)
+        {
+           var entity = _quotesDbContext.Quotes.Find(id);
+            if(entity == null)
+            {
+                return NotFound();
+            } else
+            {
+                entity.Author = quote.Author;
+                entity.Description = quote.Description;
+                entity.Title = quote.Title;
+                entity.Type = quote.Type;
+                _quotesDbContext.SaveChanges();
+                return Ok("Record updated successfully...");
+            }
+        }
+
+        // DELETE api/<QuotesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if(quote == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _quotesDbContext.Quotes.Remove(quote);
+                _quotesDbContext.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+}
---
+++ QuotesApi/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using QuotesApi.Data;
+
+namespace QuotesApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc().AddXmlSerializerFormatters();
+            services.AddDbContext<QuotesDbContext>(option => option.UseSqlServer(@"Data Source = (localdb)\ProjectsV13;Initial Catalog=QuotesDb;"));
+            services.AddResponseCaching();
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = "https://dev-6v9k8ceq.us.auth0.com/";
+                options.Audience = "https://localhost:44332/";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, QuotesDbContext quotesDbContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseResponseCaching();
+            app.UseAuthentication();
+
+            quotesDbContext.Database.EnsureCreated();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
